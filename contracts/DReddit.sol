56cc49a8f4380fa594b5b86b3220b451813e9094
Pascal Precht <pascal.precht@gmail.com>
2019-01-28 13:52:06 +0100

    step 3: introduce up and down voting

--- contracts/DReddit.sol
+++ contracts/DReddit.sol
@@ -2,10 +2,15 @@ pragma solidity ^0.5.0;
 
 contract DReddit {
 
+  enum Ballot { NONE, UPVOTE, DOWNVOTE }
+
   struct Post {
     uint creationDate;   
     bytes description;   
     address owner;
+    uint upvotes;
+    uint downvotes;
+    mapping(address => Ballot) voters;
   }
 
   Post [] public posts;
@@ -16,16 +21,42 @@ contract DReddit {
     bytes description
   );
 
+  event NewVote(
+    uint indexed postId,
+    address owner,
+    uint8 vote
+  );
+
   function createPost(bytes memory _description) public {
     uint postId = posts.length++;
 
     posts[postId] = Post({
       creationDate: block.timestamp,
       description: _description,
-      owner: msg.sender
+      owner: msg.sender,
+      upvotes: 0,
+      downvotes: 0
     });
 
     emit NewPost(postId, msg.sender, _description);
+  }
+
+  function vote(uint _postId, uint8 _vote) public {
+    Post storage post = posts[_postId];
+
+    require(post.creationDate != 0, "Post does not exist");
+    require(post.voters[msg.sender] == Ballot.NONE, "You already voted on this post");
+
+    Ballot ballot = Ballot(_vote);
+
+    if (ballot == Ballot.UPVOTE) {
+        post.upvotes++;
+    } else {
+        post.downvotes++;
+    }
+
+    post.voters[msg.sender] = ballot;
+    emit NewVote(_postId, msg.sender, _vote);
   }
 }
 
