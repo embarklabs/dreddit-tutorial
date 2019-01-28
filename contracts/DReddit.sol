7de9fa38981882a8f77ec85ead2246efc31232fe
Pascal Precht <pascal.precht@gmail.com>
2019-01-28 12:35:32 +0100

    step 2: add NewPost event

--- contracts/DReddit.sol
+++ contracts/DReddit.sol
@@ -10,13 +10,22 @@ contract DReddit {
 
   Post [] public posts;
 
+  event NewPost(
+    uint indexed postId,
+    address owner,
+    bytes description
+  );
+
   function createPost(bytes memory _description) public {
     uint postId = posts.length++;
+
     posts[postId] = Post({
       creationDate: block.timestamp,
       description: _description,
       owner: msg.sender
     });
+
+    emit NewPost(postId, msg.sender, _description);
   }
 }
 
