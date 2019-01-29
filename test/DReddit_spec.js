0915ce8ede9264efb8ea3e332b6ea99a7db95cdb
Pascal Precht <pascal.precht@gmail.com>
2019-01-29 12:31:12 +0100

    step 8: test correctness of data

--- test/DReddit_spec.js
+++ test/DReddit_spec.js
@@ -1,12 +1,16 @@
 const DReddit = embark.require('Embark/contracts/DReddit');
 
 const ipfsHash = 'Qmc5gCcjYypU7y28oCALwfSvxCBskLuPKWpK4qpterKC7z';
+let accounts = [];
 
 config({
   contracts: {
     DReddit: {}
   }
-})
+}, (err, _accounts) => {
+  accounts = _accounts;
+});
+
 
 contract('DReddit', () => {
 
@@ -19,5 +23,11 @@ contract('DReddit', () => {
     const event = receipt.events.NewPost;
     postId = event.returnValues.postId;
     assert.equal(web3.utils.toAscii(event.returnValues.description), ipfsHash);
+  });
+
+  it ('post should have correct data', async () => {
+    const post = await DReddit.methods.posts(postId).call();
+    assert.equal(web3.utils.toAscii(post.description), ipfsHash);
+    assert.equal(post.owner, accounts[0]);
   });
 });
