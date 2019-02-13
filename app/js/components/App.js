6e983eb81b5a8b3a3e4b529924a4c1db8f743ed6
Pascal Precht <pascal.precht@gmail.com>
2019-02-13 14:41:06 +0100

    step 12: render first component

---
+++ app/js/components/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+
+  render() {
+    return <h1>DReddit</h1>
+  }
+}
--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,11 @@
 import EmbarkJS from 'Embark/EmbarkJS';
+import React from 'react';
+import { render } from 'react-dom';
+import { App } from './components/App';
 
 // import your contracts
 // e.g if you have a contract named SimpleStorage:
 //import SimpleStorage from 'Embark/contracts/SimpleStorage';
+
+render(<App />, document.getElementById('root'));
 
